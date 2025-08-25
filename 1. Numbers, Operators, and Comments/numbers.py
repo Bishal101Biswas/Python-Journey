2698561ee25b8f5f9e8b295e8130ea7892f058ce
Bishal101Biswas <bb4184650@gmail.com>
2025-08-25 23:07:37 +0530

    Adding number, operators, and comments folder

---
+++ 1. Numbers, Operators, and Comments/numbers.py
@@ -0,0 +1,27 @@
+''' Python has two main types of numbers
+    
+    1. Integers
+    2. Floating Point (a.k.a. Decimal)
+'''
+
+# This is an example of Integer numbers in Python
+
+a = 2
+b = 99
+c = -22
+d = 40781181275677189554211         # The actual number written is 0781181275677189554211. Look, how many digits pop's up ?
+e = -222618
+
+print("This is an example of Integer in python :", a, b, c, d, e)
+
+print("\n\n-------------------------------------------------------------------------------\n\n")
+
+# This is an example of Floating Numbers in Python
+
+f = 2.11
+g = -11.4712
+h = 3.14161278375
+i = -99
+j = 1/3             # The answer will produce a decimal numbers upto a certain place
+
+print("This is an example of Floating Point numbers in python :", f, g, h, i, j)
