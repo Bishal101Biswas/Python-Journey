8d6d769536eb8d903601b969c51f448f95c98206
Bishal101Biswas <bb4184650@gmail.com>
2025-08-25 23:42:23 +0530

    Showing how numbers work in Python

---
+++ 1. Numbers, Operators, and Comments/1_numbers.py
@@ -0,0 +1,36 @@
+''' Python has two main types of numbers
+    
+    1. Integers
+    2. Floating Point (a.k.a. Decimal)
+'''
+
+# This is an example of Integer numbers in Python
+
+a = 2
+b = 99
+c = -22
+d = 40781181275677189554211         # The actual number written is 0781181275677189554211. Look, how many digits pop's up ?
+e = -222618
+
+print("This is an example of Integer in python :", a, b, c, d, e)
+
+print("\n\n-------------------------------------------------------------------------------\n\n")
+
+# This is an example of Floating Numbers in Python
+
+f = 2.11
+g = -11.4712
+h = 3.14161278375
+i = -99
+j = 1/3             # The answer will produce a decimal numbers upto a certain place
+
+print("This is an example of Floating Point numbers in python :", f, g, h, i, j)
+
+
+
+
+
+print("\n\n--------------------------------------------------------------------------------------\n\n")
+
+
+print("If you want to know which type of number youre dealing with then there's an easy way to find out about that.\nJust type \'type()\' inside your code editor or IDE and then enter the number inside the brackets. Your python \ninterpreter will tell you which type of number youre dealing with.\n\n\t\te.g.\n\n\t\t\t$ type(-19374.55)\n\nwill give you an answer something like this\n\n\t\t\t", type(-19374.55))
