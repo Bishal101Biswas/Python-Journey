bf51de70d0be7d0ff3e3f5416a914b1ddd6b5307
Bishal101Biswas <bb4184650@gmail.com>
2025-08-25 23:43:17 +0530

    Working with operators in Python

---
+++ 1. Numbers, Operators, and Comments/2_operations.py
@@ -0,0 +1,31 @@
+''' In Python you can use Mathematical operators i.e. +, -, *, /, even some more operators like (%) this one as well.
+    
+    Below we'll discuss what some of those new operators do in and how are they used inside Python'''
+
+a = 27523
+b = 38353
+c = a + b
+
+
+print("\nHere we'll learn about Operators such as +, -, *, / and more\n")
+print("\nFor addition we use + , Such as 27523 + 38353 = ", c,  "\n")
+print("+ is also used in Concatenation i.e. adding some variables to an existing string using +")
+print("Also a tip to remember. Adding a floating point number to an integer will result in floating point number")
+print("\n\n")
+print("\n\n")
+print("\n\n")
+print("\n\n")
+print("\n\n")
+print("\n\n")
+print("\n\n")
+print("\n\n")
+print("\n\n")
+print("\n\n")
+print("\n\n")
+print("\n\n")
+print("\n\n")
+print("\n\n")
+print("\n\n")
+print("\n\n")
+print("\n\n")
+print("\n\n")
